336c24af03a617c47ca8473f25d19c16673c7d69
Reuben E. Lecea <reuben.lecea@gmail.com>
2013-09-14 00:08:56 -0500

    gg2g

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ AudioCalc::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
