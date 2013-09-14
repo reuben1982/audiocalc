04d80418d18c621d0a3584e8d0f8d4d37e71b0a7
Reuben E. Lecea <reuben.lecea@gmail.com>
2013-09-14 07:17:42 -0500

    config.serve_static_assets = true to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,13 @@ AudioCalc::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   #config.assets.css_compressor = :sass
 
-
+  config.serve_static_assets = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.initialize_on_precompile = false
