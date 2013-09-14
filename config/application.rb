1e9aaddf8f991c416c99a0f066ef8482f297bce3
Reuben E. Lecea <reuben.lecea@gmail.com>
2013-09-14 07:11:06 -0500

    font awsom

--- config/application.rb
+++ config/application.rb
@@ -24,3 +24,7 @@ module AudioCalc
     # config.i18n.default_locale = :de
   end
 end
+config.assets.precompile += ["fontawesome-webfont.ttf",
+                             "fontawesome-webfont.eot",
+                             "fontawesome-webfont.svg",
+                             "fontawesome-webfont.woff"]
