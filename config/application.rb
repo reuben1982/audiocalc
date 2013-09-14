f8e8235c23e66a0b4c6b84498f0ea753b4405876
Reuben E. Lecea <reuben.lecea@gmail.com>
2013-09-14 05:49:32 -0500

    tried to run rake , had to move config.asset line under class appliction

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 
 module AudioCalc
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
